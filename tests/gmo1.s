5b1d1a650124333d7f01e02bb772a29d3c5a9f0e
Niranjan MR <niranjan@ashoo.in>
2025-12-09 07:51:24 +0000

    Add examples of GMO and PPO
    Example of Global memory order and
    preserved memory order
    
    Signed-off-by: Niranjan MR <niranjan@ashoo.in>

---
+++ tests/gmo1.s
@@ -0,0 +1,19 @@
+# Example of committing to global memory order
+	.section .data
+n1:
+	.word 52
+	
+	.section .text
+	.globl _start
+_start:
+	li t0, 42
+	la t1, n1 	# load address of n1 to t0
+	sw t0, 0(t1) 	# we are overriting the value of memory from 52 to 42
+	# use fence to commit to global memory object
+	fence rw, rw
+	
+	lw a0, 0(t1) 	# lets check if the value 
+	
+exit:
+	li x17, 93
+	ecall
